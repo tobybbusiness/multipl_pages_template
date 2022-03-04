d4d3d36f7a74376c86533e5927239502d9552ab2
tobybbusiness <98420517+tobybbusiness@users.noreply.github.com>
2022-03-04 23:01:14 +0000

    Update index.vue

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="blog">
     <div class="py-8 md:py-16 text-center">
-      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Blog</h1>
+      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Completed Projects</h1>
       <h2 class="text-base md:text-lg lg:text-xl xl:text-2xl">
-        Slow-carb messenger bag mlkshk fingerstache four dollar toast.
+        Below is a sample of some of our completed projects. Feel free to take inspiration from these and contact us for specific requirements.
       </h2>
     </div>
 
